1d9adbd8764e14f31ec99042d47955c5b6bf5bb2
Thomas Løkkeborg <thomahl@stud.ntnu.no>
2018-02-01 10:33:11 +0100

    #6 - setup basic structure of how rewriteengine will cooperate with twig'

--- index.php
+++ index.php
@@ -1,42 +1,56 @@
 <?
 
+require_once 'vendor/autoload.php';
 /*
  * Entry-point to the entire site. Users are shown the sites they want using 
  * the "page" GET paramter (RewriteRule makes this transparent to the user).
  */
 
-// If page is unset show index page
+
+// loader used to fetch files for twig
+$loader = new Twig_Loader_Filesystem(__DIR__. '/templates');
+
+// setup twig
+$twig = new Twig_Environment($loader, array());
+
+/**
+ * Used during rendering of page to select which twig file to render
+ * This variable is set during parsing of the $page GET parameter
+ * Null by default because during development we want the site to crash properly
+ */
+$twig_file_to_render = null;
+
+/**
+ * Used during rendering of page to send arguments to twig
+ * This variable is set during parsing of the $page GET parameter
+ */
+$twig_arguments = array();
+
+// If page is unset show index page, if it is set load correct page based on it
 if (!isset($_GET['page'])) {
-    echo '
-        <p>Index page</p>
-        <ul>
-            <li><a href="./admin">Goto admin page</a></li>
-            <li><a href="./videos">Goto videos page</a></li>
-            <li><a href="./lecturer">Goto lecturer page</a></li>
-            <li><a href="./playlists">Goto playlist page</a></li>
-        </ul>
-    ';
-    exit();
+
+    $twig_file_to_render = 'index.twig';
+
+} else {
+
+    // page stores parameter passed by GET. Contains an indication of what 
+    // page to be shown
+    $page = $_GET['page'];
+
+    // Switch on page (DEBUG: just indicate that it's working)
+    switch ($page) {
+    case 'admin':
+        $twig_file_to_render = 'debug.twig';
+        $twig_arguments = array('message' => 'admin page');
+        break;
+    case 'videos':
+        $twig_file_to_render = 'debug.twig';
+        $twig_arguments = array('message' => 'vidoes page');
+        break;
+    default:
+        $twig_file_to_render = 'notfound.twig';
+    }
 }
 
-// page stores parameter passed by GET. Contains an indication of what page to 
-// be shown
-$page = $_GET['page'];
-
-// Switch on page (DEBUG: just indicate that it's working)
-switch ($page) {
-case 'admin':
-    echo 'Admin page';
-    break;
-case 'videos':
-    echo 'Videos page';
-    break;
-case 'lecturer':
-    echo 'Lecturer page';
-    break;
-case 'playlists':
-    echo 'Playlist page';
-    break;
-default:
-    echo '404 - page not found';
-}
+// Render page
+echo $twig->render($twig_file_to_render, $twig_arguments);
